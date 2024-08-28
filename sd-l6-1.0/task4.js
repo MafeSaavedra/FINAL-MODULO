32358c0c64cf866caa8cfc730aa884c84a650458
MafeSaavedra <mariafernanda.saavedra@genstudents.org>
2024-08-27 19:54:20 -0500

    Modulo Final

--- sd-l6-1.0/task4.js
+++ sd-l6-1.0/task4.js
@@ -2,12 +2,11 @@
 import { getServerURL } from './task1.js';
 import fetch from 'node-fetch';
 
-// Función para eliminar un usuario
-export async function delUser(id) {
-    // URL del servidor JSON
-    const url = `http://localhost:3000/users/${id}`; // Cambia esto a la URL de tu servidor JSON
 
-    // Realiza la solicitud DELETE para eliminar el usuario con el ID dado
+export async function delUser(id) {
+    
+    const url = `http://localhost:3000/users/${id}`; 
+    
     const response = await fetch(url, {
         method: 'DELETE',
         headers: {
